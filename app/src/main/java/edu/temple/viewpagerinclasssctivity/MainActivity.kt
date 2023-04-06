9d90a97e0b8da4509e879366d2ed0dbd3fb47324
jzcodess <tuq67092@temple.edu>
2023-04-06 15:50:49 -0400

    4/6/2023

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,23 +2,35 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.widget.Button
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
+    private val newButton : Button by lazy{
+        findViewById(R.id.button)
+    }
+
     private val viewPager2 : ViewPager2 by lazy{
         findViewById(R.id.viewPager)
     }
+
+    var numberOfPages = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        newButton.setOnClickListener{
+            numberOfPages++
+            viewPager2.adapter?.notifyItemInserted(numberOfPages -1)
+            viewPager2.setCurrentItem(numberOfPages, false)
+
+        }
 
         viewPager2.adapter = object: FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
+            override fun getItemCount() = numberOfPages
 
             override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
         }
